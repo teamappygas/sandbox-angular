302e95431ca935e9f7378089e0717869f4485f54
Michael Lohier <lohiermichael@gmail.com>
2020-10-07 14:54:26 +0200

    Implement component inputs

--- angular_beginner_tutorial/angular-course-1-components/src/app/course-card/course-card.component.ts
+++ angular_beginner_tutorial/angular-course-1-components/src/app/course-card/course-card.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course: Course;
 
   constructor() { }
 
